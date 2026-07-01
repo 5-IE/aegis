2d29f2b8925feef32f046430a577d3b63f5014df
William Antoline <williamantoline2004@gmail.com>
2026-07-01 15:09:57 +0800

    initial commit

---
+++ Aegis/Aegis/AegisApp.swift
@@ -0,0 +1,17 @@
+//
+//  AegisApp.swift
+//  Aegis
+//
+//  Created by William Antoline's Workspace on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AegisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
